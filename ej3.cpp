6b0e2e8500b8c78703ac3f2657f7c4c28396fe5a
Maximo-AN <matimo1302@gmail.com>
2026-09-16 20:51:37 -0600

    Tarea propuesta.

---
+++ ej3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int anio;
+	printf("Digita el anio: ");
+	scanf("%d", &anio);
+
+	int resto19 = anio % 19;
+	int resto4 = anio % 4;
+	int resto7 = anio % 7;
+	int resto30 = (19 * resto19 + 24) % 30;
+	int restoPascua = (2 * resto4 + 4 * resto7 + 6 * resto30 + 5) % 7;
+
+	int dia = 22 + resto30 + restoPascua;
+	if (dia <= 31) {
+		printf("%d de marzo\n", dia);
+	} else {
+		printf("%d de abril\n", dia - 31);
+	}
+
+	return 0;
+}
